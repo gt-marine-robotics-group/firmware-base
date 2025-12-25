c6f516d2faf4546945a253d6854dcc71b81085ae
Jason Hsiao <jasonhsiao99@gmail.com>
2025-12-24 19:12:07 -0500

    Created Library Directory README

--- PIOStarter/src/main.cpp
+++ PIOStarter/src/main.cpp
@@ -3,9 +3,10 @@
 #include "config.h"
 #include "AppCore.h"
 
-
+// This is just the main test, but can eventually be something like presto for clarity
 AppCore myApp;
 
+// Don't do anything here, try and configure in AppCore for maximum flexibility and cleanliness
 void setup() {
     myApp.begin();
 }
